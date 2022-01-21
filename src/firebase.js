99043a6244485f7116f3d2a04f40bf955245e07e
hyeon1520 <hyeon_0225@naver.com>
2022-01-21 23:46:44 +0900

    firebase reset, sent moduel

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBQLdwYM_vJv50KYCvbYXQdNOT945pBaX0",
+    authDomain: "nwitter-32c65.firebaseapp.com",
+    projectId: "nwitter-32c65",
+    storageBucket: "nwitter-32c65.appspot.com",
+    messagingSenderId: "127689823933",
+    appId: "1:127689823933:web:6d7b26e576e406bf5f09d4"
+  };
+
+  export default firebaseConfig.initializeApp(firebaseConfig);
