2a4f4dccb466ddc705d3513e340a80ec11d652ea
hyeon1520 <hyeon_0225@naver.com>
2022-01-28 01:21:49 +0900

    .

--- src/fbase.js
+++ src/fbase.js
@@ -1,12 +1,9 @@
 import firebase from "firebase/compat/app";
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-
-const app = initializeApp(firebaseConfig);
-export const authService = getAuth();
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_APT_KEY,
+    apiKey: process.env.REACT_APP_API_KEY,
     authDomain: process.env.REACT_APP_AUTH_DOMAIN,
     projectId: process.env.REACT_APP_PROJECT_ID,
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
@@ -14,8 +11,6 @@ const firebaseConfig = {
     appId: process.env.REACT_APP_APP_ID
   };
 
-  //export default firebase.initializeApp(firebaseConfig);
+export default firebase.initializeApp(firebaseConfig);
 
-  firebase.initializeApp(firebaseConfig);
-
-  export const authService = firevase.auth();
+export const authService = firebase.auth();
